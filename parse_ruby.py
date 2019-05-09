90d671350542ee06e267a75c184bb15696c204eb
TRUNG NGUYEN <nhht77@gmail.com>
2019-05-09 23:42:06 +0300

    Rename python function and var

--- parse_ruby.py
+++ parse_ruby.py
@@ -1,22 +1,22 @@
 import os, sys
-from utils import (getTargetPath, getRubyFiles, getTargetsPath, getValidDev,
-                   getDev, createInternalDevList, createExternalDevList, getProjectPath)
+from utils import (get_target_path, get_ruby_files, get_ruby_targets_path, get_valid_dev,
+                   get_dev, create_internal_dev_list, create_external_dev_list, get_project_path)
 
-project_path = getProjectPath(sys)
+project_path = get_project_path(sys)
 
 # Get all ruby files from the project path in an array
-Ruby_Files = getRubyFiles(project_path)
+Ruby_Files = get_ruby_files(project_path)
 
 # Get the absolute target path of each ruby file in an array
-Path = getTargetsPath(Ruby_Files)
+Ruby_files_target = get_ruby_targets_path(Ruby_Files)
 
 # Get the dependency files of each ruby file as a dictionary
-dev = getDev(Path)
+dev = get_dev(Ruby_files_target)
 
 # Formatting the dependency file
-newDev = getValidDev(Path, dev, "'", "")
-validDev = getValidDev(Path, newDev, "/", "\\")
+new_dev = get_valid_dev(Ruby_files_target, dev, "'", "")
+valid_dev = get_valid_dev(Ruby_files_target, new_dev, "/", "\\")
 
 # creating the list of internal and external file
-createInternalDevList(sys.argv[1], validDev)
-createExternalDevList(sys.argv[1], validDev)
+create_internal_dev_list(sys.argv[1], valid_dev)
+create_external_dev_list(sys.argv[1], valid_dev)
--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import os, sys
 # INPUT   : Ruby file
 # OUTPUT  : Absolute source path of a ruby file
 # PURPOSE : Return a absolute src path of a ruby file
-def getProjectPath(sys):
+def get_project_path(sys):
     if(len(sys.argv) < 2 or len(sys.argv[1]) == 0):
         raise ValueError("Please enter the project path.")
     return sys.argv[1]
@@ -11,13 +11,13 @@ def getProjectPath(sys):
 # INPUT   : Ruby file
 # OUTPUT  : Absolute source path of a ruby file
 # PURPOSE : Return a absolute src path of a ruby file
-def getTargetPath(p):
+def get_target_path(p):
     return ("/" + os.path.realpath(p).replace("\\", "/"))
 
 # INPUT  : PATH in CMD 
 # OUTPUT : Array of ruby files
 # PURPOSE: This func receive the path in order to sort out all ruby files and return an array of ruby files
-def getRubyFiles(argv):
+def get_ruby_files(argv):
     Ruby_Files = []
     for root, dirs, files in os.walk(argv):
         for file in files:
@@ -29,38 +29,38 @@ def getRubyFiles(argv):
 # INPUT  : Array of ruby files
 # OUTPUT : Array of absolute path to the ruby files
 # PURPOSE: Return an array of absolute path of each ruby file in the received directory
-def getTargetsPath(Ruby_Files):
-    return [getTargetPath(rb)[1:] for rb in Ruby_Files]
+def get_ruby_targets_path(Ruby_Files):
+    return [get_target_path(rb)[1:] for rb in Ruby_Files]
 
 # INPUT  : An array of absolute path of each ruby files 
 # OUTPUT : a dictionary contains absolute ruby files and dependency that belongs to the files
 # PURPOSE: Find out if the ruby files has any dependency, then sort them accordingly
-def getDev(Path):
+def get_dev(ruby_files_target):
     dev = dict()
-    for p in Path:
-        dev[p]=[]
+    for rb in ruby_files_target:
+        dev[rb]=[]
    
-    for p in Path:
-        lines = [line.rstrip('\n') for line in open(p)]
-        dev[p] = [ line.replace("require ", "") for line in lines if ("require" in line)]  
+    for rb in ruby_files_target:
+        lines = [line.rstrip('\n') for line in open(rb)]
+        dev[rb] = [ line.replace("require ", "") for line in lines if ("require" in line)]  
     return dev
 
 # INPUT  : Array of Ruby files, dictionary of ruby file and dependency, invalid keyword to remove, valid keyword to replace  
 # OUTPUT : A dictionary of absolute ruby files and valid dependency file format
 # PURPOSE: Format the dependency to be searchable in the dictionary of ruby files
-def getValidDev(Path, dev, kw1, kw2):
-    newDev = dict()
-    for p in Path:
-        newDev[p]=[]
+def get_valid_dev(ruby_files_target, dev, kw1, kw2):
+    valid_dev = dict()
+    for rb in ruby_files_target:
+        valid_dev[rb]=[]
 
     for key, value in dev.items():
-        newDev[key] = [i.replace(kw1, kw2) for i in value]  
-    return newDev
+        valid_dev[key] = [i.replace(kw1, kw2) for i in value]  
+    return valid_dev
 
 # INPUT  : PATH argv from the CMD, dictionary of ruby files and an array of dependency belongs to each ruby files
 # OUTPUT : A list of internal files
 # PURPOSE: Produces a list of ​internal file-level dependencies.
-def createInternalDevList(argv, dev):
+def create_internal_dev_list(argv, dev):
     list = []
     for root, dirs, files in os.walk(argv):
         for file in files:
@@ -70,12 +70,12 @@ def createInternalDevList(argv, dev):
                     if val in os.path.join(root, file):
                         f = os.path.join(root, file)
                         list.append("/"+key+":/"+f.replace("\\", "/")+":required_internal")
-    createList_v1(argv, list, "internal_deps")
+    create_list(argv, list, "internal_deps")
 
 # INPUT  : PATH argv from the CMD, dictionary of ruby files and an array of dependency belongs to each ruby files
 # OUTPUT : A list of exertnal files
 # PURPOSE: Produces a list of external file-level dependencies.
-def createExternalDevList(argv, dev):
+def create_external_dev_list(argv, dev):
     list = []
     for root, dirs, files in os.walk(argv):
         f = None
@@ -84,12 +84,12 @@ def createExternalDevList(argv, dev):
                 for val in value:
                     if val not in os.path.join(root, file):
                         list.append("/" + key + ":/External​/" + val.replace("\\", "/") + ":required_external")
-    createList_v1(argv, list, "external_deps")
+    create_list(argv, list, "external_deps")
 
 # INPUT  : PATH argv from the CMD, Array of files, type of dependency
 # OUTPUT : the list file with information of file and its dependency
 # PURPOSE: Create the list file with information of file and its dependency
-def createList_v1(argv, Files, dev_type):
+def create_list(argv, Files, dev_type):
     Project_Name = os.path.basename(sys.argv[1])
     with open(Project_Name + "_" + dev_type + ".list", "w+", encoding="utf-8") as file:
         for p in Files:
